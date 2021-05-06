da6455d66d4edf9a29ddaf66b561f6f51eb7b521
kscheltema <82137766+kscheltema@users.noreply.github.com>
2021-05-06 17:04:15 +0200

    custom TextInput component

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import Colors from "../constants/Colors";
 import Card from "../components/Card";
+import Input from "../components/Input";
 
 const StartScreen = (props) => {
   return (
@@ -9,7 +10,14 @@ const StartScreen = (props) => {
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLenght={2}
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.buttonSize}>
             <Button title="Reset" onPress={() => {}} color={Colors.accent} />
@@ -42,10 +50,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     width: "100%",
     justifyContent: "space-between",
-    paddingHorizontal: 15,
+    padding: 15,
   },
   buttonSize: {
     width: "40%",
+  },
+  input: {
+    width: 50,
+    textAlign: "center",
   },
 });
 
